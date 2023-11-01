93da75b8adfb428d001806827f798d39992a7e8d
Chris <almeidachristian94@hotmail.com>
2023-10-31 21:20:14 -0300

    Add the basic operations of the calculator
    Made the calculator work with the basic ops and the equal sign. Alpha state

--- javascript.js
+++ javascript.js
@@ -1,6 +1,16 @@
-let primerDigito = 0;
-let segundoDigito = 0;
-let moverDigito = false;
+let primerDigito;
+let segundoDigito;
+let simbolo;
+let moverDigito;
+let firstTime = 1;
+
+function resetGlobalVariables()
+{
+    primerDigito = 0;
+    segundoDigito = 0;
+    simbolo = "";
+    moverDigito = false;
+}
 
 function initializeCalculator()
 {
@@ -11,10 +21,19 @@ function initializeCalculator()
             getButton(botones[i].textContent);
         });
         botones[i].addEventListener('keydown',(event)=>{
-            let texto = botones.map(item => item.textContent);            
+            let texto = botones.map(item => item.textContent);  
+            texto.push("*");   
+            if(event.key === "Enter")
+            {
+                event.preventDefault();
+                getButton("=");
+            }
+                      
             if(texto.includes(event.key))
             {
-                if(event.key == '*') //no anda este if
+                if(event.key === "/")
+                    event.preventDefault();
+                if(event.key === "*") 
                     getButton("X");
                 else
                     getButton(event.key);
@@ -28,6 +47,27 @@ function updateNumber(number,unit)
     return number * 10 + unit;
 }
 
+function calculateOp()
+{
+    if(!primerDigito || !segundoDigito || !simbolo)
+        return;
+    let res;
+    switch(simbolo)
+    {
+        case '+': res = primerDigito + segundoDigito;
+            break;
+        case '-':   res = primerDigito - segundoDigito;
+            break;
+        case 'X': case '*': res = primerDigito * segundoDigito;
+        break;
+        case '/': res = primerDigito / segundoDigito;
+        break;
+    }
+    resetGlobalVariables();
+    const display = document.getElementById("display");
+    display.textContent = res;
+}
+
 function getButton(botonApretado)
 {
     let pos = botonApretado.charCodeAt();
@@ -35,13 +75,30 @@ function getButton(botonApretado)
     
     if(pos > 47 && pos < 58)
         botonApretado = Number(botonApretado);
-    else
+    else if(pos === 42 || pos === 43 || pos === 45 || pos === 47 || pos === 88)
+    {
+        simbolo = botonApretado;
         moverDigito = !moverDigito;
+        display.textContent = display.textContent+""+simbolo; 
+        return;
+    }     
+    else if(pos === 61)
+        {
+            calculateOp();
+            return;
+        }   
+    else
+        return;
     
     if(!moverDigito)
     {
         primerDigito = updateNumber(primerDigito,botonApretado);
         display.textContent = primerDigito;
+    }
+    else
+    {
+        segundoDigito = updateNumber(segundoDigito,botonApretado);
+        display.textContent = primerDigito+""+simbolo+""+segundoDigito; 
     }
         
 }
@@ -50,11 +107,21 @@ const onOff = document.getElementById("onOff");
 onOff.addEventListener('click',()=>{
     let text = document.getElementById("display");
     if(text.textContent)
+    {
         text.textContent = "";
+        resetGlobalVariables();
+    }
+        
     else
     {
         text.textContent = 0;
-        initializeCalculator();
+        resetGlobalVariables();
+        if(firstTime)
+        {
+            initializeCalculator();
+            firstTime = 0;
+        }
+        
     }
         
 });
